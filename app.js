2cdf9fd5d3e3183d892e6f889bb65e58234db91a
dmitry.pevzner@gmail.com <fromvasya>
2022-11-22 21:01:01 +0300

    in progress

---
+++ app.js
@@ -0,0 +1,28 @@
+function displayError(error) {
+  document.body.innerHTML = ""
+  const title = document.createElement("p")
+  title.classList.add("hint")
+  title.textContent = `Error`
+  document.body.appendChild(title)
+  const errorLabel = document.createElement("p")
+  errorLabel.classList.add("hint")
+  errorLabel.textContent = `${error}`
+  document.body.appendChild(errorLabel)
+}
+
+function buyerOfferHTML(offer) {
+  const additionalProperties =["Год урожая", "Влажность, %", "Примесь сорная, %", "Примесь зерновая/масличная, %",
+    "Натура", "Клейковина, %",  "Клейковина, усл.ед.", "Число падения, с", "Белок, %", "Стекловидность, %"]
+  const additionalPropertiesHtml = additionalProperties.map( (propName) =>
+      `<span style ="flex-shrink: 0; font-size: small">${propName}:&nbsp${offer.attributes[propName]}&nbsp&nbsp</span>`
+  ).join("")
+  return `<div style="display: flex;">
+                          <span style="flex-grow: 1; font-weight: bold">${offer.attributes["Культура"]}</span>
+                          <span style="flex-shrink: 0;font-family: monospace; font-weight: bolder">${offer.price} тг</span>
+                        </div>
+                        <div style="display: flex">
+                          <span style="flex-grow: 1;">${offer.attributes["Класс"]}</span>
+                          <span style="flex-shrink: 0; font-size: smaller; font-family: monospace; font-weight: bolder">${offer.qty} кг</span>
+                        </div>
+                        <div style = "display: flex; flex-wrap: wrap;">${additionalPropertiesHtml}</div>`
+}
---
+++ serviceApi.js
@@ -0,0 +1,122 @@
+//const apiUrl = "https://ec2-18-184-77-249.eu-central-1.compute.amazonaws.com/api/v1"
+const apiUrl = "http://localhost:8080/api/v1"
+
+let tgWebAppData = null
+
+function initTgWebAppData(data) {
+  tgWebAppData = data
+}
+
+function getAccountOffers() {
+  return fetch(`${apiUrl}/account/offers`, {
+    headers: { "X-WebAppData": tgWebAppData },
+    mode: "cors"
+  })
+}
+
+function getAccountOffer(offerId) {
+  return fetch(`${apiUrl}/account/offers/${offerId}`, {
+    headers: { "X-WebAppData": tgWebAppData },
+    mode: "cors"
+  })
+}
+
+function changeAccountOffer(request) {
+  return fetch(`${apiUrl}/account/offers/${request.id}`, {
+    headers: {
+      "X-WebAppData": tgWebAppData,
+      "Content-Type":"application/json"
+    },
+    mode: "cors",
+    method: "PUT",
+    body: JSON.stringify(request)
+  })
+}
+
+function putFavorite(offerId) {
+  return fetch(`${apiUrl}/offers/favorites/${offerId}`, {
+    headers: {
+      "X-WebAppData": tgWebAppData,
+    },
+    mode: "cors",
+    method: "PUT"
+  })
+}
+
+function removeFavorite(offerId) {
+  return fetch(`${apiUrl}/offers/favorites/${offerId}`, {
+    headers: {
+      "X-WebAppData": tgWebAppData,
+    },
+    mode: "cors",
+    method: "DELETE"
+  })
+}
+
+
+function deleteAccountOffer(offerId) {
+  return fetch(`${apiUrl}/account/offers/${offerId}`, {
+    headers: { "X-WebAppData": tgWebAppData },
+    mode: "cors",
+    method: "DELETE"
+  })
+}
+
+function getOffers(favorites) {
+  if (favorites === undefined) favorites=false
+  return fetch(`${apiUrl}/offers?favorites=${favorites}`, {
+    headers: { "X-WebAppData": tgWebAppData },
+    mode: "cors"
+  })
+}
+
+function getDeals() {
+  return fetch(`${apiUrl}/deals`, {
+    headers: { "X-WebAppData": tgWebAppData },
+    mode: "cors"
+  })
+}
+
+function postDeal(request) {
+  return fetch(`${apiUrl}/deals`, {
+    headers: {
+      "X-WebAppData": tgWebAppData,
+      "Content-Type":"application/json"
+    },
+    mode: "cors",
+    method: "POST",
+    body: JSON.stringify(request)
+  })
+}
+
+function getUsers() {
+  return fetch(`${apiUrl}/users`, {
+    headers: { "X-WebAppData": tgWebAppData },
+    mode: "cors",
+  })
+}
+
+function chatWithUser(userId) {
+  return fetch(`${apiUrl}/chats/${userId}`, {
+    headers: { "X-WebAppData": tgWebAppData },
+    mode: "cors",
+    method: "PUT"
+  })
+}
+
+function askUserRoles(userId) {
+  return fetch(`${apiUrl}/users/${userId}/roles`, {
+    headers: { "X-WebAppData": tgWebAppData },
+    mode: "cors",
+    method: "PUT"
+  })
+}
+
+const offerStatus = {
+  initial: "initial",
+  active: "active",
+  dealStarted: "dealStarted",
+  dealRejected: "dealRejected",
+  completed: "completed",
+  deleted: "deleted"
+}
